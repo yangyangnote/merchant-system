5250785c9c179383241ac5b1f1c4307bf833eaaa
mac <mac@macdeMacBook-Air-2.local>
2025-06-26 10:36:35 +0800

    修复db.js中的语法错误，清理重复的password配置

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2/promise');
 const pool = mysql.createPool({
   host: 'localhost',
   user: 'yuanjiguanliyuan1',
-  password: 'Zky199205211022.', // <<-- 请将此替换为你的 MySQL root 密码
+  password: 'Zky199205211022.',
   database: 'merchant_system',
   waitForConnections: true,
   connectionLimit: 10,
